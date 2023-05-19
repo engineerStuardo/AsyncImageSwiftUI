8becf05a8a27a7318cbd758131e9f04bd22d1101
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-05-19 17:01:10 -0600

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Italo Stuardo on 19/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
