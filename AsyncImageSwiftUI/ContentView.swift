5fcf435e6373eae64409e5d3fc4e8500e0ad65e1
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-05-22 04:52:46 -0600

    working on how to handle async images in SwiftUI

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                AsyncImage(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/7/74/Earth_poster_large.jpg")!) { image in
+                    image
+                        .resizable()
+                        .frame(width: 300, height: 300, alignment: .center)
+                } placeholder: {
+                    ProgressView()
+                }
+                List(superHeroArray) { superhero in
+                    Text(superhero.name)
+                        .font(.title3)
+                        .foregroundColor(.purple)
+                }.navigationTitle(Text("Superhero Book"))
+            }
         }
-        .padding()
     }
 }
 
